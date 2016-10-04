5738f6722e1d48a718aa71e59484b0e00f0b3914
izicoder <tronnorskyrim@gmail.com>
2016-10-04 22:19:46 +0500

    Done beta version: thelife.py (I don't sure it's correct)

--- thelife.py
+++ thelife.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import curses
+from random import randint
 
 
 # cell class
@@ -62,6 +63,34 @@ class Map:
         return self.matrix[y][x]
 
 
+# the game of life logic class
+class Logic:
+    def __init__(self, gamemap):
+        self.map = gamemap
+
+    def calc_next(self):
+        for y in range(self.map.height):
+            for x in range(self.map.width):
+                cell = self.map.get_cell(x, y)
+                lives = 0
+                for nr in cell.neighbors:
+                    if not self.map.get_cell(nr[0], nr[1]).is_dead:
+                        lives += 1
+                if cell.is_dead and lives == 3:
+                    cell.is_dead = False
+                elif not cell.is_dead and 2 <= lives <= 3:
+                    cell.is_dead = False
+                else:
+                    cell.is_dead = True
+
+    def all_is_dead(self):
+        for y in range(self.map.height):
+            for x in range(self.map.width):
+                if not self.map.get_cell(x, y).is_dead:
+                    return False
+        return True
+
+
 # map printing function
 def print_map(scr, gamemap, xshf=0, yshf=0, life='#', dead=' '):
     for y in range(gamemap.height):
@@ -71,6 +100,20 @@ def print_map(scr, gamemap, xshf=0, yshf=0, life='#', dead=' '):
                 x+xshf,
                 dead if gamemap.get_cell(y, x).is_dead else life)
     scr.refresh()
+
+
+# initializing map
+def init_map(gamemap, *coords):
+    for x, y in coords:
+        gamemap.get_cell(x, y).is_dead = False
+
+
+# random initializing
+def rand_init(gamemap, count):
+    coords = (
+        (randint(0, gamemap.width-1), randint(0, gamemap.height-1))
+        for i in range(count))
+    init_map(gamemap, *coords)
 
 
 # curses exception decorator
@@ -89,14 +132,21 @@ def excwrap(fun):
 @excwrap
 def main():
     scr = curses.initscr()
-    gmap = Map(5, 5)
-
-    for y in range(-1, 2):
-        for x in range(-1, 2):
-            gmap.get_cell(x, y).is_dead = False
-
-    print_map(scr, gmap, dead='.')
-    scr.getkey()
+    MAP_WIDTH, MAP_HEIGHT = 100, 40
+    gmap = Map(MAP_WIDTH, MAP_HEIGHT)
+    glogic = Logic(gmap)
+    rand_init(gmap, int(MAP_HEIGHT*MAP_WIDTH/10))
+    while True:
+        print_map(scr, gmap)
+        glogic.calc_next()
+        if scr.getkey() == 'q':
+            scr.addstr(MAP_HEIGHT+2, 1, 'you click "q"')
+            scr.getkey()
+            break
+        elif glogic.all_is_dead():
+            scr.addstr(MAP_HEIGHT+2, 1, 'all is dead')
+            scr.getkey()
+            break
 
 if __name__ == '__main__':
     main()
