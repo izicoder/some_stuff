6f0cf89d7e7669442af4d545862cd0a9ff8f4b0a
izicoder <tronnorskyrim@gmail.com>
2016-10-10 18:56:23 +0500

    Done working version of thelife.py (just run it in console)

--- thelife.py
+++ thelife.py
@@ -6,114 +6,117 @@ from random import randint
 # cell class
 class Cell:
     def __init__(self, x, y):
-        # cell status
-        self.is_dead = True
-
-        # read only cell coordinates
         self.x, self.y = x, y
+
+        # True - alive, False - dead
+        self.state = False
+        self.next_state = False  # state for next generation
 
         # cell neighbors
         self.neighbors = (
-            (x-1, y-1),
-            (x,   y-1),
-            (x+1, y-1),
-            (x-1, y),
-            (x+1, y),
-            (x-1, y+1),
-            (x,   y+1),
-            (x+1, y+1))
+            (x-1, y-1),  # left top
+            (x,   y-1),  # top
+            (x+1, y-1),  # right top
+            (x-1, y),    # left
+            (x+1, y),    # right
+            (x-1, y+1),  # left bottom
+            (x,   y+1),  # bottom
+            (x+1, y+1))  # right bottom
 
 
-# global map class
-class Map:
+# cell map class
+class CellMap:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.matrix = []
-        for y in range(height):
-            self.matrix.append([])
-            for x in range(width):
-                self.matrix[y].append(Cell(x, y))
+        self.width, self.height = width, height
+
+        # cell matrix
+        self.matrix = [
+            [Cell(x, y) for x in range(width)]
+            for y in range(height)]
 
     def calc_coord(self, x, y):
-        x = (
-            self.width-abs(x)
-            if x < 0
-            else
-            self.width-x
-            if x >= self.width
-            else x)
+        if x < 0:
+            x = self.width - abs(x)
+        elif x >= self.width:
+            x = abs(self.width - x)
 
-        y = (
-            self.height-abs(y)
-            if y < 0
-            else
-            self.height-y
-            if y >= self.height
-            else
-            y)
+        if y < 0:
+            y = self.height - abs(y)
+        elif y >= self.height:
+            y = abs(self.height - y)
 
-        if 0 <= x < self.width and 0 <= y < self.height:
-            return x, y
-        else:
-            return self.calc_coord(x, y)
+        return x, y
 
     def get_cell(self, x, y):
         x, y = self.calc_coord(x, y)
         return self.matrix[y][x]
 
 
-# the game of life logic class
+# game logic class
 class Logic:
-    def __init__(self, gamemap):
-        self.map = gamemap
+    def __init__(self, cmap):
+        self.map = cmap
 
+    # calculate next generation
     def calc_next(self):
         for y in range(self.map.height):
             for x in range(self.map.width):
                 cell = self.map.get_cell(x, y)
-                lives = 0
-                for nr in cell.neighbors:
-                    if not self.map.get_cell(nr[0], nr[1]).is_dead:
-                        lives += 1
-                if cell.is_dead and lives == 3:
-                    cell.is_dead = False
-                elif not cell.is_dead and 2 <= lives <= 3:
-                    cell.is_dead = False
+                around = 0
+                for nx, ny in cell.neighbors:
+                    if self.map.get_cell(nx, ny).state:
+                        around += 1
+                if (not cell.state) and around == 3:
+                    cell.next_state = True
+                elif cell.state and 2 <= around <= 3:
+                    cell.next_state = True
                 else:
-                    cell.is_dead = True
+                    cell.next_state = False
 
+    # accept next generation
+    def accept_next(self):
+        for y in range(self.map.height):
+            for x in range(self.map.width):
+                cell = self.map.get_cell(x, y)
+                cell.state = cell.next_state
+
+    # if all cell is dead
     def all_is_dead(self):
         for y in range(self.map.height):
             for x in range(self.map.width):
-                if not self.map.get_cell(x, y).is_dead:
+                cell = self.map.get_cell(x, y)
+                if cell.state:
                     return False
         return True
 
+    # if nothing happens
+    def nothing_happens(self):
+        for y in range(self.map.height):
+            for x in range(self.map.width):
+                cell = self.map.get_cell(x, y)
+                if cell.state == cell.next_state:
+                    return True
+        return False
+
+
+# random map init
+def rand_init(cmap, count):
+    coords = [
+        (randint(0, cmap.width-1), randint(0, cmap.height-1))
+        for c in range(count)]
+    for x, y in coords:
+        cmap.get_cell(x, y).state = True
+
 
 # map printing function
-def print_map(scr, gamemap, xshf=0, yshf=0, life='#', dead=' '):
-    for y in range(gamemap.height):
-        for x in range(gamemap.width):
+def print_map(scr, cmap, xshf=0, yshf=0, life='#', dead=' '):
+    for y in range(cmap.height):
+        for x in range(cmap.width):
             scr.addch(
                 y+yshf,
                 x+xshf,
-                dead if gamemap.get_cell(y, x).is_dead else life)
+                life if cmap.get_cell(x, y).state else dead)
     scr.refresh()
-
-
-# initializing map
-def init_map(gamemap, *coords):
-    for x, y in coords:
-        gamemap.get_cell(x, y).is_dead = False
-
-
-# random initializing
-def rand_init(gamemap, count):
-    coords = (
-        (randint(0, gamemap.width-1), randint(0, gamemap.height-1))
-        for i in range(count))
-    init_map(gamemap, *coords)
 
 
 # curses exception decorator
@@ -128,25 +131,40 @@ def excwrap(fun):
     return wrapped
 
 
-# main function
+# test function
 @excwrap
-def main():
+def test():
     scr = curses.initscr()
-    MAP_WIDTH, MAP_HEIGHT = 100, 40
-    gmap = Map(MAP_WIDTH, MAP_HEIGHT)
-    glogic = Logic(gmap)
-    rand_init(gmap, int(MAP_HEIGHT*MAP_WIDTH/10))
+    curses.curs_set(0)
+    scr.nodelay(1)
+
+    WIN_HEIGHT, WIN_WITDH = scr.getmaxyx()
+
+    cm = CellMap(WIN_WITDH-1, WIN_HEIGHT)
+
+    logic = Logic(cm)
+
+    rand_init(cm, int(WIN_HEIGHT*WIN_WITDH/10))
+
     while True:
-        print_map(scr, gmap)
-        glogic.calc_next()
-        if scr.getkey() == 'q':
-            scr.addstr(MAP_HEIGHT+2, 1, 'you click "q"')
-            scr.getkey()
-            break
-        elif glogic.all_is_dead():
-            scr.addstr(MAP_HEIGHT+2, 1, 'all is dead')
+        print_map(scr, cm)
+
+        logic.calc_next()
+
+        if logic.all_is_dead():
+            scr.nodelay(0)
+            scr.addstr(1, 1, 'all cell is dead')
             scr.getkey()
             break
 
+        elif logic.nothing_happens():
+            scr.nodelay(0)
+            scr.addstr(1, 1, 'nothing happens here')
+            scr.getkey()
+            break
+
+        logic.accept_next()
+
+
 if __name__ == '__main__':
-    main()
+    test()
